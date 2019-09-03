6caddb5c999173c6b1851b0ff97aa0a11d251837
Coordenação de Informática <informatica@ifal.ifalpalmeira.edu.br>
2019-09-03 13:33:01 -0300

    Complete o código da calculadora simples

---
+++ calculadora.py
@@ -0,0 +1,27 @@
+#Complete as funcoes a seguir
+
+def soma(a, b):
+	#Insira o codigo aqui
+
+def subtrai(a, b):
+	#Insira o codigo aqui
+
+def multiplica(a, b):
+	#Insira o codigo aqui
+
+def divide(a, b):
+	#Insira o codigo aqui
+
+
+#Programa principal
+
+print("Calculadora simples")
+
+num1 = float(input("Insira o primeiro numero: "))
+num2 = float(input("Insira o segundo numero: "))
+
+soma(num1, num2)
+subtrai(num1, num2)
+multiplica(num1, num2)
+divide(num1, num2)
+
